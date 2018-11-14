d90b0b494c92d44d202d3839bc21bde7f391d45a
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-11-14 10:16:57 +0000

    Enable whitenoise for static file serving

--- haven/config/settings/base.py
+++ haven/config/settings/base.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- haven/config/settings/local.py
+++ haven/config/settings/local.py
@@ -11,5 +11,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Don't make life difficult for ourselves with password restrictions on dev
 AUTH_PASSWORD_VALIDATORS = []
-
-INTERNAL_IPS = env.list('INTERNAL_IPS', default=['127.0.0.1'])  # noqa
