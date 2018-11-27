fbea021dba0ed68423bc793b8ca16db759d414f8
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-11-27 12:10:19 +0000

    Set up logging for dev settings

--- haven/config/settings/dev.py
+++ haven/config/settings/dev.py
@@ -1,1 +1,25 @@
 from .base import *  # noqa
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': 'D:\home\site\wwwroot\myapp.log'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
+    }
+}
