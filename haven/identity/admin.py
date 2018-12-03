b6bf3747321abcc43fa2c368c7a61a0c7b3a7c28
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-12-03 11:08:08 +0000

    Web job to check AAD for newly created users

--- haven/identity/admin.py
+++ haven/identity/admin.py
@@ -10,11 +10,11 @@ class IdentityUserAdmin(UserAdmin):
 
     fieldsets = UserAdmin.fieldsets + (
         ('Extra fields', {
-            'fields': ['role', 'aad_status',]
+            'fields': ['role', 'aad_status']
         }),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('Extra fields', {
-            'fields': ['role', 'aad_status',]
+            'fields': ['role', 'aad_status']
         }),
     )
--- haven/identity/management/commands/handle_new_users.py
+++ haven/identity/management/commands/handle_new_users.py
@@ -1,8 +1,45 @@
+import logging
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+
+from identity.models import User
+
+
+logger = logging.getLogger(__name__)
+
+
+GRAPH_URL = 'https://graph.microsoft.com/v1.0/'
 
 
 class Command(BaseCommand):
     help = 'Check AAD for newly created users'
 
     def handle(self, *args, **options):
-        print("Checking for new users")
+        client_id = settings.SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_KEY
+        client_secret = settings.SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_SECRET
+        tenant_id = settings.SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_TENANT_ID
+
+        client = BackendApplicationClient(client_id=client_id)
+        oauth = OAuth2Session(client=client)
+        token = oauth.fetch_token(
+            token_url='https://login.microsoftonline.com/{}/oauth2/v2.0/token'.format(tenant_id),
+            client_id=client_id,
+            client_secret=client_secret,
+            scope='https://graph.microsoft.com/.default',
+        )
+        client = OAuth2Session(token=token)
+
+        pending = User.objects.filter(aad_status=User.AAD_STATUS_PENDING)
+        for user in pending:
+            upn = user.username
+            logger.debug("Looking in AAD for user with UPN %s" % upn)
+            response = client.get(GRAPH_URL + f'users/{user.username}')
+            if response.ok:
+                logger.debug("User found")
+                user.set_aad_status(user.AAD_STATUS_CREATED)
+                # Now, prompt the user to log in
+            else:
+                logger.debug("User not found")
