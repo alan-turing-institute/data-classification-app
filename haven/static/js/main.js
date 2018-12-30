9bab526a282225cc27af664d15a9f68010a19771
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-12-30 15:00:27 +0000

    Display yes/no toggles for classification

--- haven/static/js/main.js
+++ haven/static/js/main.js
@@ -2,10 +2,13 @@ var $ = require('jquery');
 global.jQuery = $;
 global.$ = $;
 require('django-formset');
+require('bootstrap-checkbox');
 
 $(function() {
   $("form .formset tbody tr").formset({
     addText: '<i class="fas fa-plus"></i> Add more',
     addCssClass: 'add-row btn btn-sm btn-outline-secondary',
   });
+
+  $(":checkbox").checkboxpicker();
 });
