d5cdbd17a2c5d8605d1f2e5172fca1fdcabc0b41
Tom Doel <tom.doel@codechoreography.co.uk>
2019-03-01 15:08:22 +0000

    Removed slots from provisioning as they don't correspond to our current usage model

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -43,10 +43,9 @@ else
     exit 1
 fi
 
-# Construct DB and slot names
-DB_NAME="${APP_NAME}_${APP_SLOT}"
-SLOT_NAME="${APP_NAME}-${APP_SLOT}"
-SAFE_HAVEN_DOMAIN="${SLOT_NAME}.azurewebsites.net"
+# Construct domain and DB name
+DB_NAME="${APP_NAME}"
+SAFE_HAVEN_DOMAIN="${APP_NAME}.azurewebsites.net"
 
 # Must have called `az login` by this point
 
@@ -69,44 +68,41 @@ echo "Creating the database"
 az sql db create --name=$DB_NAME --resource-group=$RESOURCE_GROUP --server=$SQL_SERVER_NAME
 
 echo "Setting up the server"
-# Create a deployment slot for dev
-az webapp deployment slot create --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT
-
 # Settings for dev server
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings SECRET_KEY=$SECRET_KEY
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings DATABASE_URL="mssql://$DB_USERNAME:$DB_PASSWORD@$SQL_SERVER_NAME.database.windows.net:1433/$DB_NAME"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings DJANGO_SETTINGS_MODULE='config.settings.dev'
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings ALLOWED_HOSTS="${SAFE_HAVEN_DOMAIN}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings SAFE_HAVEN_DOMAIN="${SAFE_HAVEN_DOMAIN}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings AZUREAD_OAUTH2_KEY="${AZUREAD_OAUTH2_KEY}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings AZUREAD_OAUTH2_SECRET="${AZUREAD_OAUTH2_SECRET}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings AZUREAD_OAUTH2_TENANT_ID="${AZUREAD_OAUTH2_TENANT_ID}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings USE_LDAP="${USE_LDAP}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings LDAP_SERVER="${LDAP_SERVER}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings LDAP_USER="${LDAP_USER}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings LDAP_PASSWORD="${LDAP_PASSWORD}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings SECRET_KEY=$SECRET_KEY
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings DATABASE_URL="mssql://$DB_USERNAME:$DB_PASSWORD@$SQL_SERVER_NAME.database.windows.net:1433/$DB_NAME"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings DJANGO_SETTINGS_MODULE='config.settings.dev'
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings ALLOWED_HOSTS="${SAFE_HAVEN_DOMAIN}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings SAFE_HAVEN_DOMAIN="${SAFE_HAVEN_DOMAIN}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings AZUREAD_OAUTH2_KEY="${AZUREAD_OAUTH2_KEY}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings AZUREAD_OAUTH2_SECRET="${AZUREAD_OAUTH2_SECRET}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings AZUREAD_OAUTH2_TENANT_ID="${AZUREAD_OAUTH2_TENANT_ID}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings USE_LDAP="${USE_LDAP}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings LDAP_SERVER="${LDAP_SERVER}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings LDAP_USER="${LDAP_USER}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings LDAP_PASSWORD="${LDAP_PASSWORD}"
 
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings EMAIL_HOST="${EMAIL_HOST}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings EMAIL_HOST_USER="${EMAIL_HOST_USER}"
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings EMAIL_HOST_PASSWORD="${EMAIL_HOST_PASSWORD}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings EMAIL_HOST="${EMAIL_HOST}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings EMAIL_HOST_USER="${EMAIL_HOST_USER}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings EMAIL_HOST_PASSWORD="${EMAIL_HOST_PASSWORD}"
 
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --slot $APP_SLOT --settings BASE_URL="${BASE_URL}"
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings BASE_URL="${BASE_URL}"
 
 # (Adding site extensions can't be done with az: see https://github.com/Azure/azure-cli/issues/7617)
 cat <<EOF
 To complete the deployment:
 
 1. Set up Python 3.6 site extension
-* Browse to Azure Portal -> App Services / $APP_NAME / Deployment slots / ${SLOT_NAME} / Extensions
+* Browse to Azure Portal -> App Services / $APP_NAME / Extensions
 * Click Add, then Choose Extension and select Python 3.6.4 x86. Accept the terms and install.
 
 2. Set up GitHub deployment
-* Browse to Azure Portal -> App Services / $APP_NAME / Deployment slots / ${SLOT_NAME} / Deployment Center
+* Browse to Azure Portal -> App Services / $APP_NAME / Deployment Center
 * Select GitHub and click Authorize
 * In the 'Configure' step, select:
   - Organization: 'alan-turing-institute'
   - repository: 'data-safe-haven-webapp'
-  - branch: whatever is appropriate for this slot
+  - branch: whatever is appropriate
 
 3. The webapp should now be accessible at: ${SAFE_HAVEN_DOMAIN}
 EOF
