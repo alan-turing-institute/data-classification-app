3c6d2187ea42203397e097186d2d40a33c6e34b2
Tom Doel <tom.doel@codechoreography.co.uk>
2019-04-06 18:26:37 +0100

    Improves login experience - add parameters to the Azure OAuth call so that 1) the user is able to select which Microsoft account to use or add a new one, and 2) only accounts with the DSG domain are included in the list of accounts

--- haven/identity/backends.py
+++ haven/identity/backends.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from social_core.backends.azuread_tenant import AzureADTenantOAuth2
 
 
@@ -16,3 +17,20 @@ class CustomAzureOAuth2Backend(AzureADTenantOAuth2):
                 'fullname': fullname,
                 'first_name': first_name,
                 'last_name': last_name}
+
+    def auth_extra_arguments(self):
+        """Return extra arguments needed on auth process."""
+        extra_arguments = super(AzureADTenantOAuth2, self).auth_extra_arguments()
+
+        # Adds an extra parameter prompt=select_account to the request.
+        # This allows the user to select which account to sign in with, or to
+        # add an account instead of automatically signing in with the last
+        # account. This is important for preventing automatic signin with the
+        # wrong account.
+        extra_arguments.update({'prompt': 'select_account'})
+
+        # Adds an extra parameter domain_hint which filters the list of
+        # accounts the user selects from when they have multuple Microsoft
+        # accounts
+        extra_arguments.update({'domain_hint': settings.SAFE_HAVEN_DOMAIN})
+        return extra_arguments
