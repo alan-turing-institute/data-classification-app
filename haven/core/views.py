f2b28d6a4e29c4fec89ce472fd460e20a95308b2
Tom Doel <tom.doel@codechoreography.co.uk>
2019-04-08 17:12:31 +0100

    Added an endpoint for Single Sign Out, allowing Azure to sign out the application when the user signs out of the account

--- haven/core/views.py
+++ haven/core/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import logout as auth_logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 SIGNOUT_URL = 'https://login.microsoftonline.com/{}/oauth2/logout?post_logout_redirect_uri={}'
 
@@ -10,3 +10,9 @@ def logout(request):
     auth_logout(request)
     url = SIGNOUT_URL.format(settings.SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_TENANT_ID, settings.BASE_URL)
     return HttpResponseRedirect(url)
+
+
+def sso_logout(request):
+    """Log out of application in response to single sign out from the auth provider"""
+    auth_logout(request)
+    return HttpResponse(status=200)
