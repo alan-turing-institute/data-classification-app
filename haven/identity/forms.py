3cc240940061ccec43c79c6f0738747c904eeb5d
Tom Doel <tom.doel@codechoreography.co.uk>
2019-05-27 15:49:18 +0100

    Add help text indicating that international numbers require prefix. Allow numbers in default region (GB) to be entered without a country code. Customised label and error messages.

--- haven/identity/forms.py
+++ haven/identity/forms.py
@@ -1,21 +1,37 @@
 from django import forms
 from django.conf import settings
 from django.core.exceptions import ValidationError
+from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 
 from .mixins import SaveCreatorMixin
 from .models import User
-from .remote import create_user
 
 
 class CreateUserForm(SaveCreatorMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'mobile']
-
+        widgets = {
+            'mobile': PhoneNumberInternationalFallbackWidget(
+                region=settings.PHONENUMBER_DEFAULT_REGION,
+                attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'mobile': 'Mobile phone number',
+        }
+        help_texts = {
+            'mobile': 'International numbers must start with +',
+        }
+        error_messages = {
+            'mobile': {
+                'required': 'A mobile phone number is required.',
+                'invalid': 'Enter a valid UK or international phone number.',
+            },
+        }
     def clean_email(self):
         email = self.cleaned_data['email']
         if User.objects.filter(email=email).exists():
-            raise ValidationError("Email address already exists")
+            raise ValidationError("There is already a user with this email address.")
         return email
 
     def save(self, **kwargs):
--- haven/identity/tests/test_forms.py
+++ haven/identity/tests/test_forms.py
@@ -22,6 +22,40 @@ class TestCreateUserForm:
         assert form_user.created_by == system_controller
         assert form_user.username == 'test.user@example.com'
 
+    def test_create_user_with_uk_phone(self, system_controller, settings):
+        settings.SAFE_HAVEN_DOMAIN = 'example.com'
+        form = CreateUserForm({
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'mobile': '01234567890',
+        }, user=system_controller)
+        assert form.is_valid()
+        assert form.cleaned_data['mobile'] == '+441234567890'
+
+        form_user = form.save()
+
+        form_user.refresh_from_db()
+        assert form_user.created_by == system_controller
+        assert form_user.username == 'test.user@example.com'
+
+    def test_create_user_with_international_format(self, system_controller, settings):
+        settings.SAFE_HAVEN_DOMAIN = 'example.com'
+        form = CreateUserForm({
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'mobile': '+1-541-754-3010',
+        }, user=system_controller)
+        assert form.is_valid()
+        assert form.cleaned_data['mobile'] == '+15417543010'
+
+        form_user = form.save()
+
+        form_user.refresh_from_db()
+        assert form_user.created_by == system_controller
+        assert form_user.username == 'test.user@example.com'
+
     def test_retries_duplicate_username(self, system_controller, settings):
         settings.SAFE_HAVEN_DOMAIN = 'example.com'
         User.objects.create_user(email='a@b.com', username='test.user@example.com')
@@ -50,3 +84,23 @@ class TestCreateUserForm:
 
         assert not form.is_valid()
         assert 'email' in form.errors
+
+    def test_form_invalid_if_uk_number_invalid(self, system_controller, settings):
+        settings.SAFE_HAVEN_DOMAIN = 'example.com'
+        form = CreateUserForm({
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'mobile': '0123456789',
+        }, user=system_controller)
+        assert not form.is_valid()
+
+    def test_form_invalid_if_int_number_invalid(self, system_controller, settings):
+        settings.SAFE_HAVEN_DOMAIN = 'example.com'
+        form = CreateUserForm({
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'mobile': '+4412345678',
+        }, user=system_controller)
+        assert not form.is_valid()
