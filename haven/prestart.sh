58b6342a8ea731bd65853727cf30496b003200b1
Tom Doel <tom.doel@codechoreography.co.uk>
2019-07-09 18:40:57 +0100

    Fix prestart script

--- haven/prestart.sh
+++ haven/prestart.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env sh
 
 # Collect static files
-RUN mkdir -p /app/staticfiles
-RUN python3 /app/manage.py collectstatic --noinput --clear
+mkdir -p /app/staticfiles
+python3 /app/manage.py collectstatic --noinput --clear
 
 echo Running DSH migrations...
 python3 /app/manage.py migrate
