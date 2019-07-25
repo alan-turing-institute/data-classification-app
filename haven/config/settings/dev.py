ce49323c12019a0f37be513090dd5d1c25e8e0b9
Tom Doel <tom.doel@codechoreography.co.uk>
2019-07-25 16:13:05 +0100

    Added SECURE_PROXY_SSL_HEADER detection to fix SSL redirection issues with OAuth2

--- haven/config/settings/dev.py
+++ haven/config/settings/dev.py
@@ -3,6 +3,11 @@ from .base import *  # noqa
 
 SECURE_SSL_REDIRECT = False
 # SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 
 DATABASES['default']['OPTIONS'] = {'sslmode':'require'}
 
--- haven/config/settings/production.py
+++ haven/config/settings/production.py
@@ -3,6 +3,10 @@ from .base import *  # noqa
 
 SECURE_SSL_REDIRECT = False
 # SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 DEBUG = False
 
