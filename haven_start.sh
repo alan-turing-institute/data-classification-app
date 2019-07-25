6d73169736952d1157093377117e218b46771de8
Tom Doel <tom.doel@codechoreography.co.uk>
2019-07-25 10:53:45 +0100

    Add haven_start script

---
+++ haven_start.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env sh
+
+echo STARTUP SCRIPT
+
+# Debugging: start SSH server
+#mkdir -p /var/run/sshd
+#/usr/sbin/sshd
+
+# Collect static files
+mkdir -p /home/site/repository/staticfiles
+python3 haven/manage.py collectstatic --noinput --clear
+
+echo Running DSH migrations...
+python3 haven/manage.py migrate
+
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir haven config.wsgi
+
