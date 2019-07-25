3746e80e32731b8b565738b95b1607c84ac1d5e9
Tom Doel <tom.doel@codechoreography.co.uk>
2019-07-25 22:15:15 +0100

    Change dev server settings to match production server, including: - Disable SSL web traffic since server is behind proxy - Enable SSL for database traffic - Enforce session and CSRF cookies

--- haven/config/settings/dev.py
+++ haven/config/settings/dev.py
@@ -1,8 +1,22 @@
 from .base import *  # noqa
 
+# DEBUG must be False for production deployments
+DEBUG = False
 
-SECURE_SSL_REDIRECT = True
+# Settings for using Django with a proxy
+SECURE_SSL_REDIRECT = False
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
 
+# Allows browsers to ensure cookies are secure
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Enforce SSL for database communication
+DATABASES['default'].setdefault('OPTIONS', {})
+DATABASES['default']['OPTIONS']['sslmode'] = 'require'
+
+# Log to the console so it will be captured by Azure's log stream
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -13,18 +27,16 @@ LOGGING = {
         },
     },
     'handlers': {
-        'logfile': {
+        'console': {
             'level': 'WARNING',
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': 'D:\home\site\wwwroot\haven.log',
+            'class': 'logging.StreamHandler',
             'formatter': 'verbose'
-        }
+        },
     },
     'loggers': {
         'django': {
-            'handlers': ['logfile'],
+            'handlers': ['console'],
             'level': 'WARNING',
-            'propagate': False,
-        }
+        },
     }
 }
