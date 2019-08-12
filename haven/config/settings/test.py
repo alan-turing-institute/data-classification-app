710e4656ad64c7d7b613ad2a12b8f752a50913b8
Steven Carlysle-Davies <S.Carlysle-Davies@epcc.ed.ac.uk>
2019-08-12 12:47:01 +0100

    Fix tests to not require Postgres DB

--- haven/config/settings/test.py
+++ haven/config/settings/test.py
@@ -5,6 +5,7 @@ os.environ['SECRET_KEY'] = 'notasecret'
 os.environ['AZUREAD_OAUTH2_KEY'] = 'notasecret'
 os.environ['AZUREAD_OAUTH2_SECRET'] = 'notasecret'
 os.environ['AZUREAD_OAUTH2_TENANT_ID'] = 'notasecret'
+os.environ['DATABASE_URL'] = 'sqlite:////tmp/my-tmp-sqlite.db'
 
 
 from .base import *  # noqa
