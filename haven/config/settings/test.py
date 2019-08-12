a509ece48e6d482476dc2530fabe55a3f3049e89
Steven Carlysle-Davies <S.Carlysle-Davies@epcc.ed.ac.uk>
2019-08-12 12:46:03 +0100

    Fix tests to not require haven/.env

--- haven/config/settings/test.py
+++ haven/config/settings/test.py
@@ -1,3 +1,12 @@
+import os
+
+
+os.environ['SECRET_KEY'] = 'notasecret'
+os.environ['AZUREAD_OAUTH2_KEY'] = 'notasecret'
+os.environ['AZUREAD_OAUTH2_SECRET'] = 'notasecret'
+os.environ['AZUREAD_OAUTH2_TENANT_ID'] = 'notasecret'
+
+
 from .base import *  # noqa
 
 
