c1c23ce6402a472fff1c440ce7928d475d7128ee
Steven Carlysle-Davies <S.Carlysle-Davies@epcc.ed.ac.uk>
2019-09-02 11:44:30 +0100

    Refactor tests to remove duplication

--- conftest.py
+++ conftest.py
@@ -139,22 +139,34 @@ def as_project_participant(client, project_participant):
 
 
 @pytest.fixture
+def as_investigator(client, investigator):
+    return client_login(client, investigator.user)
+
+
+@pytest.fixture
 def classified_work_package(programme_manager, investigator, data_provider_representative, referee):
     def _classified_work_package(tier):
         project = recipes.project.make(created_by=programme_manager)
         work_package = recipes.work_package.make(project=project)
+        dataset = recipes.dataset.make()
+
         project.add_user(user=investigator.user,
                          role=ProjectRole.INVESTIGATOR.value,
                          creator=programme_manager)
         project.add_user(user=data_provider_representative.user,
                          role=ProjectRole.DATA_PROVIDER_REPRESENTATIVE.value,
                          creator=programme_manager)
-        work_package.classify_as(tier, investigator.user)
-        work_package.classify_as(tier, data_provider_representative.user)
-        if tier >= Tier.TWO:
-            project.add_user(user=referee.user,
-                             role=ProjectRole.REFEREE.value,
-                             creator=programme_manager)
-            work_package.classify_as(tier, referee.user)
+        project.add_user(user=referee.user,
+                         role=ProjectRole.REFEREE.value,
+                         creator=programme_manager)
+
+        project.add_dataset(dataset, data_provider_representative.user, investigator.user)
+        work_package.add_dataset(dataset, investigator.user)
+
+        if tier is not None:
+            work_package.classify_as(tier, investigator.user)
+            work_package.classify_as(tier, data_provider_representative.user)
+            if tier >= Tier.TWO:
+                work_package.classify_as(tier, referee.user)
         return work_package
     return _classified_work_package
