83aa4ceb3688716cf951eb3d333b7892cce201b9
Steven Carlysle-Davies <S.Carlysle-Davies@epcc.ed.ac.uk>
2019-11-15 14:57:31 +0000

    Add participant list for all users (fix #233)

--- haven/projects/roles.py
+++ haven/projects/roles.py
@@ -84,7 +84,7 @@ class UserProjectPermissions:
         view_history         |  Y   Y |  Y   .  .   .   . |     .
         add_datasets         |  Y   Y |  Y   .  Y   .   . |     .
         add_work_packages    |  Y   Y |  Y   .  Y   .   . |     .
-        list_participants    |  Y   Y |  Y   .  Y   .   . |     .
+        list_participants    |  Y   Y |  Y   Y  Y   Y   Y |     .
         edit_participants    |  Y   Y |  Y   .  Y   .   . |     .
         view_classification  |  Y   Y |  Y   Y  Y   Y   . |     .
         classify_data        |  .   . |  .   Y  Y   Y   . |     .
--- haven/projects/tests/test_roles.py
+++ haven/projects/tests/test_roles.py
@@ -80,11 +80,11 @@ class TestProjectRoleListParticipants:
 
     def test_researcher_cannot_list_participants(self):
         perms = UserProjectPermissions(ProjectRole.RESEARCHER, UserRole.NONE)
-        assert not perms.can_list_participants
+        assert perms.can_list_participants
 
     def test_referee_cannot_list_participants(self):
         perms = UserProjectPermissions(ProjectRole.REFEREE, UserRole.NONE)
-        assert not perms.can_list_participants
+        assert perms.can_list_participants
 
     def test_programme_manager_can_list_participants(self):
         perms = UserProjectPermissions(ProjectRole.RESEARCHER, UserRole.PROGRAMME_MANAGER)
