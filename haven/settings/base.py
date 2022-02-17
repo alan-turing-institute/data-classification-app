73116fe5d1305b00ce33134c40e052098a19ddc4
tcouch <t.couch@ucl.ac.uk>
2022-02-17 16:33:48 +0000

    use remote user instead of python social auth

--- haven/settings/base.py
+++ haven/settings/base.py
@@ -3,4 +3,5 @@ from .components.common import *
 from .components.easyaudit import *
 from .components.email import *
 from .components.bleach import *
-from .components.python_social_auth import *
+# from .components.python_social_auth import *
+from .components.remote_user_auth import *
