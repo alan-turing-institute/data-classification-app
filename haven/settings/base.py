2c873eae928d5ed6c4db3eb2f2bd7e10e406f7c8
tcouch <t.couch@ucl.ac.uk>
2022-02-22 13:14:18 +0000

    start settings module again

--- haven/settings/base.py
+++
@@ -1,7 +0,0 @@
-# Import settings for all components
-from .components.common import *
-from .components.easyaudit import *
-from .components.email import *
-from .components.bleach import *
-# from .components.python_social_auth import *
-from .components.remote_user_auth import *
