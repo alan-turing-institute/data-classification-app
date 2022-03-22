4496f3cba2e046db71d4b63e49ea011e98e2a9db
tcouch <t.couch@ucl.ac.uk>
2022-03-22 13:53:09 +0000

    add w_packs, ppts, and dsets to project details

--- haven/projects/api/serializers.py
+++ haven/projects/api/serializers.py
@@ -4,8 +4,11 @@ from ..models import Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    participants = serializers.StringRelatedField(many=True)
+    work_packages = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Project
         fields = [
-            "name", "uuid", "description", "archived"
+            "name", "uuid", "description", "archived", "work_packages", "datasets", "participants"
         ]
