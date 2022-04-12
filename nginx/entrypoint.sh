55683a6abb6797f6a2a8fff69120019ec525bda7
tcouch <t.couch@ucl.ac.uk>
2022-04-12 11:17:14 +0100

    More templates required for includes

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,5 +2,7 @@
 set -eu
 
 envsubst '${BASE_DOMAIN}' < /etc/nginx/conf.d/dca.conf.template > /etc/nginx/conf.d/dca.conf
+envsubst '${BASE_DOMAIN}' < /etc/nginx/includes/auth.conf.template > /etc/nginx/includes/auth.conf
+envsubst '${BASE_DOMAIN}' < /etc/nginx/includes/ssl.conf.template > /etc/nginx/includes/ssl.conf
 
 exec "$@"
