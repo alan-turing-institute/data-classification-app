95826872349f024041a161e6d62d965c93dbd880
tcouch <t.couch@ucl.ac.uk>
2022-04-26 14:26:15 +0100

    Add classificationquestionset to admin

--- haven/data/admin.py
+++ haven/data/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 
-from haven.data.models import ClassificationGuidance, ClassificationQuestion, Dataset
+from haven.data.models import ClassificationGuidance, ClassificationQuestion, ClassificationQuestionSet, Dataset
 
 
 admin.site.register(ClassificationGuidance)
 admin.site.register(ClassificationQuestion, SimpleHistoryAdmin)
+admin.site.register(ClassificationQuestionSet)
 admin.site.register(Dataset)
