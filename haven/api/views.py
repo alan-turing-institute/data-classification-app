bcfbc986fbb2f1b8f61d6c31999ce75593e2432b
mitch104 <mitchell.harle@omnifarious-group.com>
2022-05-17 13:07:45 +0100

    Update Dataset model and add API for registering host and storage_path

--- haven/api/views.py
+++ haven/api/views.py
@@ -1,5 +1,6 @@
 from oauth2_provider.contrib.rest_framework import TokenHasScope
 from rest_framework import generics
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAuthenticated
 
 from haven.api.serializers import (
@@ -34,6 +35,21 @@ class ExtraFilterKwargsMixin:
         return extra_filters
 
 
+class AllowedPatchFieldsMixin:
+    """Mixin for use in DRF UpdateAPIView which defines which fields can be patched"""
+
+    # Fields which can be patched by this view
+    allowed_patch_fields = []
+    PARSE_ERROR_DETAIL = "Only the following fields can be used with PATCH for this data type: {}"
+
+    def patch(self, request, *args, **kwargs):
+        # If there are any fields that are now allowed raise error response,
+        # which result in 400 status code by default
+        if set(request.POST.keys()) - set(self.allowed_patch_fields):
+            raise ParseError(detail=self.PARSE_ERROR_DETAIL.format(self.allowed_patch_fields))
+        return super().patch(request, *args, **kwargs)
+
+
 class DatasetListAPIView(ExtraFilterKwargsMixin, generics.ListAPIView):
     """API view to return a list of datasets that the requesting user has access to"""
 
@@ -64,6 +80,20 @@ class DatasetDetailAPIView(ExtraFilterKwargsMixin, generics.RetrieveAPIView):
         return get_accessible_datasets(
             self.request._auth.user, extra_filters=self.get_filter_kwargs()
         )
+
+
+class DatasetRegisterAPIView(AllowedPatchFieldsMixin, generics.UpdateAPIView):
+    serializer_class = DatasetSerializer
+    required_scopes = ["write"]
+    permission_classes = [IsAuthenticated, TokenHasScope]
+    lookup_field = "uuid"
+    lookup_url_kwarg = "uuid"
+    # Fields which can be patched by this view
+    allowed_patch_fields = ["host", "storage_path"]
+
+    def get_queryset(self):
+        """Return all datasets accessible by requesting OAuth user"""
+        return get_accessible_datasets(self.request._auth.user)
 
 
 class ProjectListAPIView(ExtraFilterKwargsMixin, generics.ListAPIView):
