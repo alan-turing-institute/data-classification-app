93d3b84b4655cf11372e50a264bb3caefc0c6bb5
mitch104 <mitchell.harle@omnifarious-group.com>
2022-06-01 16:33:13 +0100

    Add permission checks to oauth application views

---
+++ haven/oauth_urls.py
@@ -0,0 +1,70 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.urls import re_path
+from oauth2_provider import views
+from oauth2_provider.urls import base_urlpatterns, oidc_urlpatterns
+
+from haven.identity.roles import UserRole
+
+
+app_name = "oauth2_provider"
+
+
+def can_access_oauth_application_views(user):
+    """
+    Function which returns a boolean for where or not user should be able to access the application
+    views provided by django-oauth-toolkit
+    """
+    return user.is_superuser or user.user_role.value in [
+        UserRole.SYSTEM_MANAGER.value,
+        UserRole.PROGRAMME_MANAGER.value,
+    ]
+
+
+management_urlpatterns = [
+    # Application management views
+    re_path(
+        r"^applications/$",
+        user_passes_test(can_access_oauth_application_views)(views.ApplicationList.as_view()),
+        name="list",
+    ),
+    re_path(
+        r"^applications/register/$",
+        user_passes_test(can_access_oauth_application_views)(
+            views.ApplicationRegistration.as_view()
+        ),
+        name="register",
+    ),
+    re_path(
+        r"^applications/(?P<pk>[\w-]+)/$",
+        user_passes_test(can_access_oauth_application_views)(views.ApplicationDetail.as_view()),
+        name="detail",
+    ),
+    re_path(
+        r"^applications/(?P<pk>[\w-]+)/delete/$",
+        user_passes_test(can_access_oauth_application_views)(views.ApplicationDelete.as_view()),
+        name="delete",
+    ),
+    re_path(
+        r"^applications/(?P<pk>[\w-]+)/update/$",
+        user_passes_test(can_access_oauth_application_views)(views.ApplicationUpdate.as_view()),
+        name="update",
+    ),
+    # Token management views
+    re_path(
+        r"^authorized_tokens/$",
+        user_passes_test(can_access_oauth_application_views)(
+            views.AuthorizedTokensListView.as_view()
+        ),
+        name="authorized-token-list",
+    ),
+    re_path(
+        r"^authorized_tokens/(?P<pk>[\w-]+)/delete/$",
+        user_passes_test(can_access_oauth_application_views)(
+            views.AuthorizedTokenDeleteView.as_view()
+        ),
+        name="authorized-token-delete",
+    ),
+]
+
+
+urlpatterns = base_urlpatterns + management_urlpatterns + oidc_urlpatterns
