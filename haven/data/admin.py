33f6cf36568b5717c175d4c35775629755a38091
tcouch <t.couch@ucl.ac.uk>
2022-06-06 20:09:11 +0100

    Improve related question search in admin

--- haven/data/admin.py
+++ haven/data/admin.py
@@ -4,7 +4,11 @@ from simple_history.admin import SimpleHistoryAdmin
 from haven.data.models import ClassificationGuidance, ClassificationQuestion, ClassificationQuestionSet, Dataset
 
 
+class ClassificationQuestionAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["yes_question", "no_question"]
+    search_fields = ["question_set__name", "question", "name"]
+
 admin.site.register(ClassificationGuidance)
-admin.site.register(ClassificationQuestion, SimpleHistoryAdmin)
+admin.site.register(ClassificationQuestion, ClassificationQuestionAdmin)
 admin.site.register(ClassificationQuestionSet)
 admin.site.register(Dataset)
